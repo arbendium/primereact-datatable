6a010106b551113a33b01e47c2252533405425a1
Keijo Kapp <keijo.kapp@gmail.com>
2023-12-22 22:09:20 +0200

    init fork

--- components/lib/datatable/RowCheckbox.js
+++ components/lib/datatable/RowCheckbox.js
@@ -5,58 +5,58 @@ import { useMergeProps } from '../hooks/Hooks';
 import { CheckIcon } from '../icons/check';
 import { IconUtils } from '../utils/Utils';
 
-export const RowCheckbox = React.memo((props) => {
-    const mergeProps = useMergeProps();
-    const getColumnProps = () => ColumnBase.getCProps(props.column);
-    const { ptm, ptmo, cx } = props.ptCallbacks;
+export const RowCheckbox = React.memo(props => {
+	const mergeProps = useMergeProps();
+	const getColumnProps = () => ColumnBase.getCProps(props.column);
+	const { ptm, ptmo, cx } = props.ptCallbacks;
 
-    const getColumnPTOptions = (key) => {
-        const columnMetaData = {
-            props: getColumnProps(),
-            parent: props.metaData,
-            hostName: props.hostName,
-            state: {},
-            context: {
-                index: props.tabIndex,
-                checked: props.checked,
-                disabled: props.disabled
-            }
-        };
+	const getColumnPTOptions = key => {
+		const columnMetaData = {
+			props: getColumnProps(),
+			parent: props.metaData,
+			hostName: props.hostName,
+			state: {},
+			context: {
+				index: props.tabIndex,
+				checked: props.checked,
+				disabled: props.disabled
+			}
+		};
 
-        return mergeProps(ptm(`column.${key}`, { column: columnMetaData }), ptm(`column.${key}`, columnMetaData), ptmo(getColumnProps(), key, columnMetaData));
-    };
+		return mergeProps(ptm(`column.${key}`, { column: columnMetaData }), ptm(`column.${key}`, columnMetaData), ptmo(getColumnProps(), key, columnMetaData));
+	};
 
-    const onChange = (event) => {
-        if (!props.disabled) {
-            props.onChange(event);
-        }
-    };
+	const onChange = event => {
+		if (!props.disabled) {
+			props.onChange(event);
+		}
+	};
 
-    const checkboxIconProps = mergeProps(
-        {
-            className: cx('checkIcon')
-        },
-        getColumnPTOptions('rowCheckbox.icon')
-    );
-    const icon = props.checked ? props.checkIcon || <CheckIcon {...checkboxIconProps} /> : null;
-    const checkIcon = IconUtils.getJSXIcon(icon, { ...checkboxIconProps }, { props });
-    const tabIndex = props.disabled ? null : '0';
+	const checkboxIconProps = mergeProps(
+		{
+			className: cx('checkIcon')
+		},
+		getColumnPTOptions('rowCheckbox.icon')
+	);
+	const icon = props.checked ? props.checkIcon || <CheckIcon {...checkboxIconProps} /> : null;
+	const checkIcon = IconUtils.getJSXIcon(icon, { ...checkboxIconProps }, { props });
+	const tabIndex = props.disabled ? null : '0';
 
-    const checkboxProps = mergeProps(
-        {
-            role: 'checkbox',
-            'aria-checked': props.checked,
-            tabIndex: tabIndex,
-            onChange: onChange,
-            'aria-label': props.ariaLabel,
-            checked: props.checked,
-            icon: checkIcon,
-            disabled: props.disabled
-        },
-        getColumnPTOptions('rowCheckbox')
-    );
+	const checkboxProps = mergeProps(
+		{
+			role: 'checkbox',
+			'aria-checked': props.checked,
+			tabIndex,
+			onChange,
+			'aria-label': props.ariaLabel,
+			checked: props.checked,
+			icon: checkIcon,
+			disabled: props.disabled
+		},
+		getColumnPTOptions('rowCheckbox')
+	);
 
-    return <Checkbox {...checkboxProps} />;
+	return <Checkbox {...checkboxProps} />;
 });
 
 RowCheckbox.displayName = 'RowCheckbox';
