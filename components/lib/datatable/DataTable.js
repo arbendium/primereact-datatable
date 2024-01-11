19fa2db50b4bbc3a86645435078249c99e611187
Keijo Kapp <keijo.kapp@gmail.com>
2024-01-11 18:07:51 +0200

    Simplified filtering

--- components/lib/datatable/DataTable.js
+++ components/lib/datatable/DataTable.js
@@ -22,27 +22,6 @@ import { TableHeader } from './TableHeader.js';
 const emptyArray = [];
 const getColumnProp = (column, name) => ColumnBase.getCProp(column, name);
 
-const getActiveFilters = filters => {
-	const removeEmptyFilters = ([key, value]) => {
-		if (value.constraints) {
-			const filteredConstraints = value.constraints.filter(constraint => constraint.value !== null);
-
-			if (filteredConstraints.length > 0) {
-				return [key, { ...value, constraints: filteredConstraints }];
-			}
-		} else if (value.value !== null) {
-			return [key, value];
-		}
-
-		return undefined;
-	};
-
-	const filterValidEntries = entry => entry !== undefined;
-	const entries = Object.entries(filters).map(removeEmptyFilters).filter(filterValidEntries);
-
-	return Object.fromEntries(entries);
-};
-
 export function DataTable(inProps) {
 	const context = React.useContext(PrimeReactContext);
 	const props = React.useMemo(() => DataTableBase.getProps(inProps, context), [inProps, context]);
@@ -1008,59 +987,71 @@ export function DataTable(inProps) {
 	}, [props.filterLocale]);
 
 	const filterLocal = React.useCallback((data, filters) => {
-		if (!data) return;
+		function activeFilter(value) {
+			if (value.constraints) {
+				const filteredConstraints = value.constraints.filter(constraint => constraint.value !== null);
 
-		const activeFilters = filters ? getActiveFilters(filters) : {};
-
-		let filteredValue = [];
-
-		const isGlobalFilter = activeFilters.global || props.globalFilter;
-		let globalFilterFieldsArray;
-
-		if (isGlobalFilter) {
-			globalFilterFieldsArray = props.globalFilterFields || columns.filter(col => !getColumnProp(col, 'excludeGlobalFilter')).map(col => getColumnProp(col, 'filterField') || getColumnProp(col, 'field'));
+				if (filteredConstraints.length > 0) {
+					return { ...value, constraints: filteredConstraints };
+				}
+			} else if (value.value !== null) {
+				return value;
+			}
 		}
+
+		const activeFilters = Object.entries(filters)
+			// eslint-disable-next-line array-callback-return
+			.map(([key, value]) => {
+				if (key !== 'global') {
+					const filter = activeFilter(value);
+
+					if (filter) {
+						return [key, filter];
+					}
+				}
+			})
+			.filter(entry => entry !== undefined);
+
+		const globalFilter = filters.global ? activeFilter(filters.global) : undefined;
+
+		const filteredValue = [];
+
+		const globalFilterFieldsArray = globalFilter || props.globalFilter
+			? props.globalFilterFields || columns.filter(col => !getColumnProp(col, 'excludeGlobalFilter')).map(col => getColumnProp(col, 'filterField') || getColumnProp(col, 'field'))
+			: undefined;
 
 		for (let i = 0; i < data.length; i++) {
 			let localMatch = true;
 			let globalMatch = false;
 			let localFiltered = false;
 
-			for (const prop in activeFilters) {
-				if (prop === 'null') {
-					continue;
+			for (const [filterField, filterMeta] of activeFilters) {
+				localFiltered = true;
+
+				if (filterMeta.operator) {
+					for (let j = 0; j < filterMeta.constraints.length; j++) {
+						const filterConstraint = filterMeta.constraints[j];
+
+						localMatch = executeLocalFilter(filterField, data[i], filterConstraint, j);
+
+						if ((filterMeta.operator === FilterOperator.OR && localMatch) || (filterMeta.operator === FilterOperator.AND && !localMatch)) {
+							break;
+						}
+					}
+				} else {
+					localMatch = executeLocalFilter(filterField, data[i], filterMeta, 0);
 				}
 
-				if (Object.prototype.hasOwnProperty.call(activeFilters, prop) && prop !== 'global') {
-					localFiltered = true;
-					const filterField = prop;
-					const filterMeta = activeFilters[filterField];
-
-					if (filterMeta.operator) {
-						for (let j = 0; j < filterMeta.constraints.length; j++) {
-							const filterConstraint = filterMeta.constraints[j];
-
-							localMatch = executeLocalFilter(filterField, data[i], filterConstraint, j);
-
-							if ((filterMeta.operator === FilterOperator.OR && localMatch) || (filterMeta.operator === FilterOperator.AND && !localMatch)) {
-								break;
-							}
-						}
-					} else {
-						localMatch = executeLocalFilter(filterField, data[i], filterMeta, 0);
-					}
-
-					if (!localMatch) {
-						break;
-					}
+				if (!localMatch) {
+					break;
 				}
 			}
 
-			if (localMatch && isGlobalFilter && !globalMatch && globalFilterFieldsArray) {
+			if (localMatch && !globalMatch && globalFilterFieldsArray) {
 				for (let j = 0; j < globalFilterFieldsArray.length; j++) {
 					const globalFilterField = globalFilterFieldsArray[j];
-					const matchMode = activeFilters.global ? activeFilters.global.matchMode : props.globalFilterMatchMode;
-					const value = activeFilters.global ? activeFilters.global.value : props.globalFilter;
+					const matchMode = globalFilter ? globalFilter.matchMode : props.globalFilterMatchMode;
+					const value = globalFilter ? globalFilter.value : props.globalFilter;
 
 					globalMatch = FilterService.filters[matchMode](ObjectUtils.resolveFieldData(data[i], globalFilterField), value, props.filterLocale);
 
@@ -1070,25 +1061,30 @@ export function DataTable(inProps) {
 				}
 			}
 
-			let matches;
-
-			if (isGlobalFilter) {
-				matches = localFiltered ? localFiltered && localMatch && globalMatch : globalMatch;
-			} else {
-				matches = localFiltered && localMatch;
-			}
+			const matches = globalFilterFieldsArray
+				? (localFiltered ? localFiltered && localMatch && globalMatch : globalMatch)
+				: localFiltered && localMatch;
 
 			if (matches) {
 				filteredValue.push(data[i]);
 			}
 		}
 
-		if (filteredValue.length === props.value.length || Object.keys(activeFilters).length === 0) {
-			filteredValue = data;
+		if (filteredValue.length === props.value.length) {
+			return data;
 		}
 
 		return filteredValue;
-	}, [props.globalFilter, props.globalFilterFields, columns, executeLocalFilter, props.filterLocale, props.globalFilterMatchMode, props.value]);
+	}, [
+		filters,
+		props.globalFilter,
+		props.globalFilterFields,
+		columns,
+		executeLocalFilter,
+		props.filterLocale,
+		props.globalFilterMatchMode,
+		props.value
+	]);
 
 	const cloneFilters = filters => {
 		const cloned = {};
